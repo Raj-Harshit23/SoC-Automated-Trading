473e7ddda0cf4d1c59bef3c561588344d5968238
Harshit Raj <harshitraj969@gmail.com>
2024-06-07 18:29:12 +0530

    MACD calc completed

--- task1_solution/macd.py
+++ task1_solution/macd.py
@@ -6,21 +6,35 @@ from sma import calculate_SMAs
 
 def EMA(prices,period):
     initial_sma=calculate_SMAs(prices,period)[0]
-    print(initial_sma.size)     
+    # print(initial_sma.size)     
     ema=pd.Series(np.zeros_like(prices),index=prices.index) #initialising
 
     ema.iloc[:period]=initial_sma[:period]
     multiplier=2/(period+1)
-    for t in range(period+1,len(prices)):
+    for t in range(period,len(prices)):
         ema.iloc[t]=prices.iloc[t]*multiplier+ema.iloc[t-1]*(1-multiplier)
 
     return ema
+
+def MACD(prices, fast=12, slow=26, signal=9):
+    ema_fast=EMA(prices,fast)
+    ema_slow=EMA(prices,slow)
+
+    macd_line=ema_fast-ema_slow
+
+    signal_line=EMA(macd_line,signal)
+
+    hist=macd_line-signal_line
+
+    return macd_line,signal_line,hist
 
 
 if __name__ == "__main__":
     ##Set the data parameters
     ticker='JSWENERGY.NS'
     type='Close'
-    period=14;
+    period=12;
     data=fetch_extended_data(ticker,type,period)
-    print(EMA(data,12))
+    # print(EMA(data,12))
+    print("-----")
+    print(MACD(data))
