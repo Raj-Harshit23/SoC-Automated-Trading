afcd181992ac188e4005f90dd6dbf929093dc5db
Harshit Raj <harshitraj969@gmail.com>
2024-06-07 17:56:05 +0530

    EMA calculated

--- task1_solution/macd.py
+++ task1_solution/macd.py
@@ -1,8 +1,26 @@
 import pandas as pd
 import numpy as np
 import datetime
-from getdata import data_extract
+from getdata import fetch_extended_data
 from sma import calculate_SMAs
 
 def EMA(prices,period):
-    initial_sma=calculate_SMAs(prices[:period],period)[0]
+    initial_sma=calculate_SMAs(prices,period)[0]
+    print(initial_sma.size)     
+    ema=pd.Series(np.zeros_like(prices),index=prices.index) #initialising
+
+    ema.iloc[:period]=initial_sma[:period]
+    multiplier=2/(period+1)
+    for t in range(period+1,len(prices)):
+        ema.iloc[t]=prices.iloc[t]*multiplier+ema.iloc[t-1]*(1-multiplier)
+
+    return ema
+
+
+if __name__ == "__main__":
+    ##Set the data parameters
+    ticker='JSWENERGY.NS'
+    type='Close'
+    period=14;
+    data=fetch_extended_data(ticker,type,period)
+    print(EMA(data,12))
