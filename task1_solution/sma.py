6cc096cffd2e23f7d05f0870a9f4ef0df4c85264
Harshit Raj <harshitraj969@gmail.com>
2024-06-07 18:28:31 +0530

    Updated data filling process

--- task1_solution/sma.py
+++ task1_solution/sma.py
@@ -2,13 +2,15 @@ import pandas as pd
 import numpy as np
 from getdata import fetch_extended_data
 
-def calculate_SMAs(prices, short_window=12, long_window=50):
+def calculate_SMAs(prices, short_window=20, long_window=50):
     short_ma = prices.rolling(window=short_window).mean()
     long_ma = prices.rolling(window=long_window).mean()
 
     #Data cleaning
-    short_ma.dropna(how='any',inplace=True)
-    long_ma.dropna(how='any',inplace=True)
+    # short_ma.dropna(how='any',inplace=True)
+    # long_ma.dropna(how='any',inplace=True)
+    short_ma.fillna(0,inplace=True)
+    long_ma.fillna(0,inplace=True)
 
     return short_ma, long_ma
 
@@ -18,6 +20,10 @@ if __name__ == "__main__":
     ticker='JSWENERGY.NS'
     type='Close'
     data=fetch_extended_data(ticker,type,50)
+    # print(data)
+
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
     print(calculate_SMAs(data))
 
 
